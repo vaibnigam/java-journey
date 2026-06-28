c22fa272214b1ff8399fcb80826065256fd954f2
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-06-29 03:32:18 +0530

    Data Types

---
+++ Day3_DataTypes/NonPrimitive.java
@@ -0,0 +1,14 @@
+public class NonPrimitive {
+	public static void main (String args[]) {
+		String s = null;
+		int[] arry = {0,1};
+		String t = new String();
+		t="vaibhav";
+System.out.println(arry[0]);
+System.out.println(t);
+System.out.println(s);
+
+enum Day {MONDAY , tuesday , Wednesday};
+Day d = Day.tuesday;
+System.out.println(d);
+}}
---
+++ Day3_DataTypes/Primitive.java
@@ -0,0 +1,23 @@
+public class Primitive {
+	public static void main (String args[]) {
+		int a = 127;
+		float b = 3.14f;
+		double c = 1.23456789;
+		byte d = 127;
+		byte e = -128;
+		long f = 123123123123L;
+		short g = 32167;
+		char h = 'Q';
+		boolean i = true;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+System.out.println(i);
+	}
+}
+		
